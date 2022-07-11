1b9723e4500993bd0e07ba8dfeaa77fb9e32ea3e
Alberto Lopez <albertoojeda44@aragon.unam.mx>
2022-07-11 01:13:35 -0500

    prueba

---
+++ src/main/java/com/cinlat/CinlatRekogniteApplication.java
@@ -0,0 +1,15 @@
+package com.cinlat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CinlatRekogniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinlatRekogniteApplication.class, args);
+	}
+	
+
+}
---
+++ src/test/java/com/cinlat/CinlatRekogniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinlat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinlatRekogniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
